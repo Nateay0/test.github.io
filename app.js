53d5ee5dcf37a892fed7be699307710a83ec05c2
Nathan K <92893096+Natesplanet@users.noreply.github.com>
2021-11-02 20:49:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ particlesJS("particles-js", {
             },
         },
         color: {
-            value: "#1db954",
+            value: "#FFFFFF",
         },
         shape: {
             type: "circle",
@@ -50,7 +50,7 @@ particlesJS("particles-js", {
         line_linked: {
             enable: true,
             distance: 150,
-            color: "#1db954",
+            color: "#FFFFFF",
             opacity: 0.4,
             width: 1,
         },
